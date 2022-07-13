1b4d79048abe6f24c14d66ca7beb18eb2c4e853c
Ryan Klotzbach <ryanklotzbach@Ryans-iMac.local>
2022-07-12 20:46:37 -0700

    updated code

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+# Title: **Read Me Generator Example**
+
+  # License Type:
+  ![Apache_2.0](https://img.shields.io/badge/License-Apache_2.0-important.svg)
+
+  # Table of Contents
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+  # Description:
+  Create a read me generator 
+
+  # Installation:
+  example instructions
+
+  # Usage:
+  example usage
+
+  # Contributing:
+  example text
+
+  # Tests:
+  example test text
+
+  # Questions:
+  ### GitHub: user@github.com
+
+  ### Email: user@mail.com
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,52 +1,44 @@
 // TODO: Include packages needed for this application
-
 const inquirer = require("inquirer");
 const fs =require("fs");
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        message: "What is the project's title?",
-        name: "title",
-    },
-    {
-        message : "Please enter a description of the project.",
-        name: "description",
-    },
-    {
-        message: "What is the installation?",
-        name: "installation",
-    },
-    {
-        message: "What is the usage?",
-        name: "usage",
-    },
-    {
-        message: "Who else is contributing?",
-        name: "contributing",
-    },
-    {
-        message: "What are the tests?",
-        name: "tests",
-    },
-    {
-        message: "What is your GitHub username?",
-        name: "github",
-    },
-    {
-        message: "Please enter your email",
-        name: "email"
-    }
-
+    {message: "What is the project's title?",
+    name: "title"},
+    {type: "list",
+    message: "What is the license type?",
+    choices: ["Apache_2.0", "Boost_1.0", "BSD_2--Clause", "MIT"],
+    name: "license"},
+    {message : "Please enter a description of the project.",
+    name: "description"},
+    {message: "What is the installation?",
+    name: "installation"},
+    {message: "What is the usage?",
+    name: "usage"},
+    {message: "Who else is contributing?",
+    name: "contributing"},
+    {message: "What are the tests?",
+    name: "tests"},
+    {message: "What is your GitHub username?",
+    name: "github"},
+    {message: "Please enter your email",
+    name: "email"}
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile("README.md", generateMarkdown(data), (err) => (err) ? console.log("There is an Error in the Title") : console.log("Title is Successful!"))
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile(data);
+        generateMarkdown(data.title);
+    })
 }
 
 // Function call to initialize app
